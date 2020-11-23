be9b34107488e2f57ef6871e1b0ec0268a7eb78b
X-TermiNation <64397435+X-TermiNation@users.noreply.github.com>
2020-11-23 15:35:05 +0700

    rating tiap film udh ada

--- viewMovie.php
+++ viewMovie.php
@@ -217,8 +217,130 @@ $rating = $conn->query($queryratingfilm)->fetch_all(MYSQLI_ASSOC);
                             color: white;
                         }
                     </style>
+                    <h1>Rate this film</h1>
+
+                    <div class="rating">
+                        <!--
+    --><input name="stars" id="e5" type="radio"></a><label for="e5">☆</label>
+                        <!--
+          --><input name="stars" id="e4" type="radio"></a><label for="e4">☆</label>
+                        <!--
+          --><input name="stars" id="e3" type="radio"></a><label for="e3">☆</label>
+                        <!--
+          --><input name="stars" id="e2" type="radio"></a><label for="e2">☆</label>
+                        <!--
+          --><input name="stars" id="e1" type="radio"></a><label for="e1">☆</label>
+                    </div>
+
                 </div>
             </div>
+            <style>
+                body {
+                    background: #FDFDFD;
+                    font-family: "Segoe WP", "Segoe UI", Helvetica, Arial, sans-serif;
+                    text-align: center;
+                }
+
+                h1,
+                h2 {
+                    color: #888;
+                    margin: 0;
+                    font-weight: normal;
+                }
+
+                h1 {
+                    padding-top: 1.5em;
+                    padding-bottom: 2em;
+                }
+
+                h2 {
+                    clear: both;
+                    color: #aaa;
+                    padding: 2em 0 0.3em
+                }
+
+                em {
+                    display: block;
+                    margin: .5em auto 2em;
+                    color: #bbb;
+                }
+
+                p,
+                p a {
+                    color: #aaa;
+                    text-decoration: none;
+                }
+
+                p a:hover,
+                p a:focus {
+                    text-decoration: underline;
+                }
+
+                p+p {
+                    color: #bbb;
+                    margin-top: 2em;
+                }
+
+                .detail {
+                    position: absolute;
+                    text-align: right;
+                    right: 5px;
+                    bottom: 5px;
+                    width: 50%;
+                }
+
+                a[href*="intent"] {
+                    display: inline-block;
+                    margin-top: 0.4em;
+                }
+
+                /*  
+		 * Rating styles
+		 */
+                .rating {
+                    width: 226px;
+                    margin: 0 auto 1em;
+                    font-size: 45px;
+                    overflow: hidden;
+                    margin-left: 550px;
+                }
+
+                .rating input {
+                    float: right;
+                    opacity: 0;
+                    position: absolute;
+                }
+
+                .rating a,
+                .rating label {
+                    float: right;
+                    color: #aaa;
+                    text-decoration: none;
+                    -webkit-transition: color .4s;
+                    -moz-transition: color .4s;
+                    -o-transition: color .4s;
+                    transition: color .4s;
+                }
+
+                .rating label:hover~label,
+                .rating input:focus~label,
+                .rating label:hover,
+                .rating a:hover,
+                .rating a:hover~a,
+                .rating a:focus,
+                .rating a:focus~a {
+                    color: orange;
+                    cursor: pointer;
+                }
+
+                .rating2 {
+                    direction: rtl;
+                }
+
+                .rating2 a {
+                    float: none
+                }
+            </style>
         </div>
         <footer>
             <p style="text-align: center;">&copy;Pemrograman Web</p>
